db47836af15cef4d8f894c140e22be348f5c6c35
lyf2000 <lyf2000@mail.ru>
2020-05-18 08:17:26 +0300

    JWT get\refresh

--- backend/users/api/urls.py
+++ backend/users/api/urls.py
@@ -2,14 +2,18 @@ from django.urls import path
 from rest_framework import routers
 
 from blog.api.views import PostViewSet, TagViewSet
+
 # from users.api.views import UserViewSet
+from users.api.tokens import TokenCreate, TokenRefresh
 
 app_name = 'api'
-
 
 router = routers.DefaultRouter()
 # router.register('users', UserViewSet, basename='user-api')
 
 urlpatterns = [
     # path('users/<int:pk>', UserRetrieveView.as_view(), name='user'),
+    path('token/', TokenCreate.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefresh.as_view(), name='token_refresh'),
 ] + router.urls
+
