9632b768107276a3d03028dd8b067208911951b3
lyf2000 <lyf2000@mail.ru>
2020-05-20 10:02:27 +0300

    header

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
             activate, name='activate'),
     path('login/', SignInnView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(template_name='users/logged_out.html'), name='logged-out'),
+    path('logout/', LogoutView.as_view(template_name='users/logged_out.html'), name='logout'),
     path('reset/', Reset.as_view(), name='reset'),
     path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
