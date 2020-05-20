4bc16920b527c7daccde21b257572d0af86dacdf
lyf2000 <lyf2000@mail.ru>
2020-05-20 17:23:19 +0300

    create meet title

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -174,7 +174,7 @@ class SetPasswordForm(setPasswordForm):
     new_password1 = forms.CharField(
         label=_("New password"),
         widget=forms.PasswordInput(
-            attrs={'placeholder': 'Email'}
+            attrs={'placeholder': 'New password'}
         ),
         strip=False,
         # help_text=password_validation.password_validators_help_text_html(),
